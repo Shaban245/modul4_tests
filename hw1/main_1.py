3f298a24e00f7952d03a90af6c334abed60b55f6
shaban <shaban.elaev@mail.ru>
2025-03-07 14:47:29 +0300

    hw1/main1 fix; function equation

--- hw1/main_1.py
+++ hw1/main_1.py
@@ -1,13 +1,21 @@
 import math
 
+
+b = int(input("b >>> "))
+a = int(input("a >>> "))
+c = int(input("c >>> "))
+
+
 def equation(a: int, b: int, c:int) -> int or str:
     discriminant = b**2 - a * c * 4
     if discriminant > 0:
         x1 = (-b + math.sqrt(discriminant)) / (2 * a)
         x2 = (-b - math.sqrt(discriminant)) / (2 * a)
-        return x1, x2
+        return f"x1 = {x1}, x2 = {x2}"
     elif discriminant == 0:
         x = -b / (2 * a)
-        return x
+        return f"x = {x}"
     else:
-        return print("у уравнения нет корней")
+        return "у уравнения нет корней"
+
+print(equation(a, b , c))
